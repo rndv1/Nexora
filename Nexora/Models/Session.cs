ed55c3cce9cd0266d5ccf87bffc0680cd769367a
Timur Murtazin <timur.murtazin0@gmail.com>
2026-05-09 04:31:34 +0300

    Добавить ApplicationDbContext

--- Nexora/Models/Session.cs
+++ Nexora/Models/Session.cs
@@ -3,7 +3,8 @@
     public class Session
     {
         public int UserId { get; set; }
-        public string Token { get; set; }
+        public string Token { get; set; } = string.Empty;
         public DateTime ExpiresAt { get; set; }
+        public User? User { get; set; }
     }
 }
--- Nexora/Models/User.cs
+++ Nexora/Models/User.cs
@@ -3,8 +3,12 @@
     public class User
     {
         public int Id { get; set; }
-        public string Login { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Account? Account { get; set; }
+        public Session? Session { get; set; }
+        public ICollection<Transaction> SentTransactions { get; set; } = new List<Transaction>();
+        public ICollection<Transaction> ReceivedTransactions { get; set; } = new List<Transaction>();
     }
 }   
