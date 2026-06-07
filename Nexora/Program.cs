bfc409a05b8ce10c3ab58fbda1d9bc24660447f9
Timur Murtazin <timur.murtazin0@gmail.com>
2026-06-07 16:55:03 +0300

    Протестировать регистрацию через Swagger

--- Nexora/Program.cs
+++ Nexora/Program.cs
@@ -20,16 +20,20 @@ namespace Nexora
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IAccountService, AccountService>();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
