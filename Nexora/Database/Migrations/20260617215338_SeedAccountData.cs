126dc2eba00324dae1a124aee576474cbdf25dd5
Timur Murtazin <timur.murtazin0@gmail.com>
2026-06-18 01:36:29 +0300

    Добавить Seed-данные для аккаунтов

---
+++ Nexora/Database/Migrations/20260617215338_SeedAccountData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Nexora.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAccountData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "accounts",
+                columns: new[] { "Id", "Balance", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1000m, 1 },
+                    { 2, 2000m, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
